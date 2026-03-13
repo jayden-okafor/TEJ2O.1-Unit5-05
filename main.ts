7881498cc9311f52678ffca0338470dc41ad6d3d
jayden-okafor <jayden.okafor@stu.ocsb.ca>
2026-03-13 12:11:29 -0400

    changedd ts code

--- main.ts
+++ main.ts
@@ -1,8 +1,26 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2026 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jayden Okafor
+ * Created on: Mar 2026
+ * This program measures the distance using the sonar.
 */
 
-basic.showString('Hello, World!')
+// variable
+let distanceToObject:number = 0
+basic.clearScreen()
+
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+
+    distanceToObject = sonar.ping(
+       DigitalPin.P1,
+       DigitalPin.P2,
+       PingUnit.Centimeters,
+    )
+basic.showNumber(distanceToObject)
+basic.showIcon(IconNames.Happy)
+})
+
+
