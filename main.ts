19fe9f1e0dfa662ac6dd39a3a8c70053157cf842
jayden-okafor <jayden.okafor@stu.ocsb.ca>
2026-03-26 11:52:17 -0400

    added comments

--- main.ts
+++ main.ts
@@ -9,17 +9,19 @@
 let distanceToObject:number = 0
 basic.clearScreen()
 
+// show happy face
 basic.showIcon(IconNames.Happy)
 
+// when button "A" is clicked
 input.onButtonPressed(Button.A, function() {
     basic.clearScreen()
 
+// measure the distance in cm
     distanceToObject = sonar.ping(
        DigitalPin.P1, // trigger
        DigitalPin.P2, // echo
        PingUnit.Centimeters,
-    )
-    
-basic.showNumber(distanceToObject)
+    )   
+basic.showString(distanceToObject.toString() + ' cm')
 basic.showIcon(IconNames.Happy)
 })
